3b91fbde7688c6192841e9f4b6ad482369424ccf
Ritik kumar <ritikkumar8@amityonline.com>
2024-10-28 03:32:20 +0530

    emoji add

--- src/components/Others/Header.jsx
+++ src/components/Others/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div className="flex items-end justify-between m-4">
-        <h1 className="text-2xl font-bold"> <span className='text-xl'>Hello</span><br />Mr Roy</h1>
+        <h1 className="text-2xl font-bold"> <span className='text-xl'>Hello</span><br />Mr Roy 👋</h1>
         <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Log Out</button>
     </div>
   )
